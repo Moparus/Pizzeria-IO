d0a2a77277edb9ebe4a5a64908464acd2f968211
Moparus <74992069+Moparus@users.noreply.github.com>
2025-06-04 20:13:56 +0200

    Dodanie przykładowych zamówień
    Do ułatwienia testów

--- Pizzeria/Pizzeria.cpp
+++ Pizzeria/Pizzeria.cpp
@@ -49,6 +49,39 @@ int main() {
     pracownicy.push_back(new Pracownik("Piotr", "Zielinski", "kierowca", "kierowca"));
     pracownicy.push_back(new Pracownik("Stefan", "Szef", "szef", "szef"));
 
+    // Menu i lista zamówień
+    Menu menu;
+    ListaZamowien listaZamowien;
+
+    const auto& pozycjeMenu = menu.pobierzPozycje();
+    if (pozycjeMenu.size() >= 2) { // Sprawdź czy są pozycje w menu
+        // Zamówienie 1
+        Klient* k1 = new Klient("Adam", "Nowak", "Warszawa", "ul. Kwiatowa 1");
+        listaZamowien.dodajZamowienie("telefoniczne", time(nullptr) - 3600 * 24, time(nullptr) - 3600 * 23, k1, pracownicy[0], "nowe");
+        listaZamowien.pobierzWszystkie().back()->dodajElementZamowienia(pozycjeMenu[0], 2);
+
+        // Zamówienie 2
+        Klient* k2 = new Klient("Beata", "Kowalczyk", "Kraków", "ul. Słoneczna 5");
+        listaZamowien.dodajZamowienie("przykasie", time(nullptr) - 3600 * 20, time(nullptr) - 3600 * 19, k2, pracownicy[0], "gotowe");
+        listaZamowien.pobierzWszystkie().back()->dodajElementZamowienia(pozycjeMenu[1], 1);
+
+        // Zamówienie 3
+        Klient* k3 = new Klient("Cezary", "Lis", "Poznań", "ul. Polna 7");
+        listaZamowien.dodajZamowienie("telefoniczne", time(nullptr) - 3600 * 10, time(nullptr) - 3600 * 9, k3, pracownicy[0], "w trakcie dostawy");
+        listaZamowien.pobierzWszystkie().back()->dodajElementZamowienia(pozycjeMenu[0], 1);
+
+        // Zamówienie 4
+        Klient* k4 = new Klient("Dorota", "Mazur", "Gdańsk", "ul. Leśna 3");
+        listaZamowien.dodajZamowienie("przykasie", time(nullptr) - 3600 * 5, time(nullptr) - 3600 * 4, k4, pracownicy[0], "wydane");
+        listaZamowien.pobierzWszystkie().back()->dodajElementZamowienia(pozycjeMenu[1], 3);
+
+        // Zamówienie 5
+        Klient* k5 = new Klient("Ewa", "Bąk", "Wrocław", "ul. Długa 10");
+        listaZamowien.dodajZamowienie("telefoniczne", time(nullptr) - 3600 * 2, time(nullptr) - 3600 * 1, k5, pracownicy[0], "nowe");
+        listaZamowien.pobierzWszystkie().back()->dodajElementZamowienia(pozycjeMenu[0], 1);
+        listaZamowien.pobierzWszystkie().back()->dodajElementZamowienia(pozycjeMenu[1], 2);
+    }
+
     while (true) {
         clearScreen();
         cout << "==== Pizzeria Konsola ====" << endl;
